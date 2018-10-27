a04050fa27982f2302dfa75c01a2270e5e3a1f0e
Helt <Heltman@163.com>
2018-10-27 21:35:22 +0800

    测试idea提交
    Signed-off-by: Helt <Heltman@163.com>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args){
+        print();
+    }
+
+    public static void print() {
         System.out.println("Hello world!");
         System.out.println("测试更新内容!");
         System.out.println("测试更新内容!");
