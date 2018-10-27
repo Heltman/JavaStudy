dc138fb4f78db0243f8a5ab1a9a341936958e193
Helt <Heltman@163.com>
2018-10-27 20:40:30 +0800

    Signed-off-by: Helt <Heltman@163.com>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("测试更新内容!");
+        System.out.println("测试更新内容!");
     }
 }
